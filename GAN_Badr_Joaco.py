5e3ffdbc8c0f4ea4b3e38dfee8737461bbf82daf
LBadr <61498622+LBadr@users.noreply.github.com>
2021-04-07 15:29:11 +0200

    Add files via upload

---
+++ GAN_Badr_Joaco.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  6 23:05:37 2021
+
+@author: lyazi
+"""
+
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+
+
+def sample_Z(m, n):
+    return np.random.uniform(-1., 1., size=[m, n])
+
+def plot(samples):
+    fig = plt.figure(figsize=(4, 4))
+    gs = gridspec.GridSpec(4, 4)
+    gs.update(wspace=0.05, hspace=0.05)
+
+    for i, sample in enumerate(samples):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+
+    return fig
+
+
+X = tf.placeholder(tf.float32, shape=[None, 784])
+
+
+Z = tf.placeholder(tf.float32, shape=[None, 100])
+
+def generator(z):
+    with tf.variable_scope("generator", reuse=tf.AUTO_REUSE):
+        x = tf.layers.dense(z, 128, activation=tf.nn.relu)
+        x = tf.layers.dense(z, 784)
+        x = tf.nn.sigmoid(x)
+    return x
+
+def discriminator(x):
+    with tf.variable_scope("discriminator", reuse=tf.AUTO_REUSE):
+        x = tf.layers.dense(x, 128, activation=tf.nn.relu)
+        x = tf.layers.dense(x, 1)
+        x = tf.nn.sigmoid(x)
+    return x
+
+
+G_sample = generator(Z)
+
+
+D_real = discriminator(X)
+D_fake = discriminator(G_sample)
+
+
+
+D_loss = -tf.reduce_mean(tf.log(D_real) + tf.log(1. - D_fake))
+G_loss = -tf.reduce_mean(tf.log(D_fake))
+
+
+disc_vars = [var for var in tf.trainable_variables() if var.name.startswith("disc")]
+gen_vars = [var for var in tf.trainable_variables() if var.name.startswith("gen")]
+
+
+D_solver = tf.train.AdamOptimizer().minimize(D_loss, var_list=disc_vars)
+G_solver = tf.train.AdamOptimizer().minimize(G_loss, var_list=gen_vars)
+
+
+
+mb_size = 128
+
+
+Z_dim = 100
+
+mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+if not os.path.exists('out2/'):
+    os.makedirs('out2/')
+
+i = 0
+
+for it in range(1000000):
+
+    
+    if it % 1000 == 0:
+        samples = sess.run(G_sample, feed_dict={Z: sample_Z(16, Z_dim)})
+
+        fig = plot(samples)
+        plt.savefig('out2/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
+        i += 1
+        plt.close(fig)
+
+
+    
+    X_mb, _ = mnist.train.next_batch(mb_size)
+
+
+    
+    _, D_loss_curr = sess.run([D_solver, D_loss], feed_dict={X: X_mb, Z: sample_Z(mb_size, Z_dim)})
+
+    
+    _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z: sample_Z(mb_size, Z_dim)})
+
+    
+    if it % 1000 == 0:
+        print('Iter: {}'.format(it))
+        print('D loss: {:.4}'. format(D_loss_curr))
